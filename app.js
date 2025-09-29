e55c0ee49a8931d07568f0928cd52a4edc898a2a
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-09-29 14:31:54 +0530

    user roete created using express.Router()

--- app.js
+++ app.js
@@ -2,24 +2,31 @@
 import express from "express";
 
 const app = express();
+const router = express.Router();
 const PORT = 3000;
 
 // HTTP method handling
 app.get("/", (req, res) => {
-  res.send("Hello Express!");
+  res.send("Hello Express, this is test!");
 });
 
-app
-  .route("/book")
-  .get((req, res) => {
-    res.send("Get a random book");
-  })
-  .post((req, res) => {
-    res.send("Add a book");
-  })
-  .put((req, res) => {
-    res.send("Update the book");
-  });
+// app
+//   .route("/book")
+//   .get((req, res) => {
+//     res.send("Get a random book");
+//   })
+//   .post((req, res) => {
+//     res.send("Add a book");
+//   })
+//   .put((req, res) => {
+//     res.send("Update the book");
+//   });
+
+router.get("/users", (req, res) => {
+  res.send("This is GET user...");
+});
+
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
